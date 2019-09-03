7382d28b432737a21edc706d68996d71d3745673
Mariana Secu <secu.mariana92@gmail.com>
2019-09-03 17:44:34 -0400

    Handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES('BBQ Bacon WHOPPER', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Big Fish', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Crispy Chicken', true);
+INSERT INTO burgers (burger_name, devoured) VALUES('Veggie burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES('Steak burger', true);
